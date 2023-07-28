a199febc7c5772130ba51f4404406c3f79959c39
Marlon Rosenberg <sceleton.smasher@gmail.com>
2023-07-29 00:36:39 +0200

    test: Add unit test for DatabaseConnection

--- src/test/java/timetracker/API/DatabaseConnectionTest.java
+++ src/test/java/timetracker/API/DatabaseConnectionTest.java
@@ -18,100 +18,100 @@ class DatabaseConnectionTest {
     void testDataProjects() {
 
         Project project = new Project(-2, "JUnitTest", "An Test Project", null);
-        GlobalVariables.PROJECT_MAP.remove(-2);
+        GlobalVariables.ID_TO_PROJECT_MAP.remove(-2);
 
         // write
         dataWriter.writeProject(project);
         assertTrue(dataReader.readAllProjects().contains(project));
-        assertTrue(GlobalVariables.PROJECT_MAP.containsValue(project));
+        assertTrue(GlobalVariables.ID_TO_PROJECT_MAP.containsValue(project));
 
         //  update
         project.setName("JUnitTestUpdate");
         dataWriter.updateProject(project);
-        GlobalVariables.PROJECT_MAP.remove(-2);
+        GlobalVariables.ID_TO_PROJECT_MAP.remove(-2);
         assertTrue(dataReader.readAllProjects().contains(project));
-        assertEquals(project.getName(), GlobalVariables.PROJECT_MAP.get(-2).getName());
+        assertEquals(project.getName(), GlobalVariables.ID_TO_PROJECT_MAP.get(-2).getName());
 
         // remove
         dataRemover.removeProject(project);
-        GlobalVariables.PROJECT_MAP.remove(-2);
+        GlobalVariables.ID_TO_PROJECT_MAP.remove(-2);
         assertFalse(dataReader.readAllProjects().contains(project));
-        assertNull(GlobalVariables.PROJECT_MAP.get(-2));
+        assertNull(GlobalVariables.ID_TO_PROJECT_MAP.get(-2));
     }
 
     @Test
     void testDataTags() {
         Tag tag = new Tag(-2, "JUnitTest", null, Color.BLUE);
-        GlobalVariables.TAG_MAP.remove(-2);
+        GlobalVariables.ID_TO_TAG_MAP.remove(-2);
 
         // write
         dataWriter.writeTag(tag);
         assertTrue(dataReader.readAllTags().contains(tag));
-        assertTrue(GlobalVariables.TAG_MAP.containsValue(tag));
+        assertTrue(GlobalVariables.ID_TO_TAG_MAP.containsValue(tag));
 
         // update
         tag.setName("JUnitTestUpdate");
         dataWriter.updateTag(tag);
-        GlobalVariables.TAG_MAP.remove(-2);
+        GlobalVariables.ID_TO_TAG_MAP.remove(-2);
         assertTrue(dataReader.readAllTags().contains(tag));
-        assertEquals(tag.getName(), GlobalVariables.TAG_MAP.get(-2).getName());
+        assertEquals(tag.getName(), GlobalVariables.ID_TO_TAG_MAP.get(-2).getName());
 
         // remove
         dataRemover.removeTag(tag);
-        GlobalVariables.TAG_MAP.remove(-2);
+        GlobalVariables.ID_TO_TAG_MAP.remove(-2);
         assertFalse(dataReader.readAllTags().contains(tag));
-        assertNull(GlobalVariables.TAG_MAP.get(-2));
+        assertNull(GlobalVariables.ID_TO_TAG_MAP.get(-2));
     }
 
     @Test
     void testDataTasks() {
         Task task = new Task(-2, "JUnitTest", null);
-        GlobalVariables.TASK_MAP.remove(-2);
+        GlobalVariables.ID_TO_TASK_MAP.remove(-2);
 
         // write
         dataWriter.writeTask(task);
         assertTrue(dataReader.readAllTasks().contains(task));
-        assertTrue(GlobalVariables.TASK_MAP.containsValue(task));
+        assertTrue(GlobalVariables.ID_TO_TASK_MAP.containsValue(task));
 
         // update
         task.setName("JUnitTestUpdate");
         dataWriter.updateTask(task);
-        GlobalVariables.TASK_MAP.remove(-2);
+        GlobalVariables.ID_TO_TASK_MAP.remove(-2);
         assertTrue(dataReader.readAllTasks().contains(task));
-        assertEquals(task.getName(), GlobalVariables.TASK_MAP.get(-2).getName());
+        assertEquals(task.getName(), GlobalVariables.ID_TO_TASK_MAP.get(-2).getName());
 
         // remove
         dataRemover.removeTask(task);
-        GlobalVariables.TASK_MAP.remove(-2);
+        GlobalVariables.ID_TO_TASK_MAP.remove(-2);
         assertFalse(dataReader.readAllTasks().contains(task));
-        assertNull(GlobalVariables.TASK_MAP.get(-2));
+        assertNull(GlobalVariables.ID_TO_TASK_MAP.get(-2));
 
     }
 
     @Test
     void testDataTimeIntervals() {
         TimeInterval timeInterval = new TimeInterval(-2, new Task(-2, "JUnitTest", null));
-        GlobalVariables.TIME_INTERVAL_MAP.remove(-2);
+        GlobalVariables.ID_TO_TIME_INTERVAL_MAP.remove(-2);
         timeInterval.start();
         timeInterval.stop();
 
         // write
         dataWriter.writeTimeInterval(timeInterval);
         assertTrue(dataReader.readAllTimeIntervals().contains(timeInterval));
-        assertTrue(GlobalVariables.TIME_INTERVAL_MAP.containsValue(timeInterval));
+        assertTrue(GlobalVariables.ID_TO_TIME_INTERVAL_MAP.containsValue(timeInterval));
 
         // update
         timeInterval.setStartTime(LocalDateTime.now());
         dataWriter.updateTimeInterval(timeInterval);
-        GlobalVariables.TIME_INTERVAL_MAP.remove(-2);
+        GlobalVariables.ID_TO_TIME_INTERVAL_MAP.remove(-2);
         assertTrue(dataReader.readAllTimeIntervals().contains(timeInterval));
-        assertEquals(timeInterval.getStartTime(), GlobalVariables.TIME_INTERVAL_MAP.get(-2).getStartTime());
+        assertEquals(timeInterval.getStartTime(), GlobalVariables.ID_TO_TIME_INTERVAL_MAP.get(-2).getStartTime());
 
         // remove
         dataRemover.removeTimeInterval(timeInterval);
-        GlobalVariables.TIME_INTERVAL_MAP.remove(-2);
+        GlobalVariables.ID_TO_TIME_INTERVAL_MAP.remove(-2);
         assertFalse(dataReader.readAllTimeIntervals().contains(timeInterval));
-        assertNull(GlobalVariables.TIME_INTERVAL_MAP.get(-2));
+        assertNull(GlobalVariables.ID_TO_TIME_INTERVAL_MAP.get(-2));
     }
 
     @Test
