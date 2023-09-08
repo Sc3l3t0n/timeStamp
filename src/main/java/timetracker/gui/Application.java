198a72a78a7b0317a8b9d18ccde8590539a450ce
Marlon Rosenberg <sceleton.smasher@gmail.com>
2023-09-09 00:14:30 +0200

    wip: Add features to Application
    - collect method
    - shutdown hook

--- src/main/java/timetracker/gui/Application.java
+++ src/main/java/timetracker/gui/Application.java
@@ -5,7 +5,16 @@ import timetracker.API.DataReader;
 public class Application{
 
     public Application() {
+        collect();
         MainForm mainForm = new MainForm();
+        Runtime.getRuntime().addShutdownHook(new Thread()
+        {
+            @Override
+            public void run()
+            {
+                System.out.println("Shutdown hook ran!"); //TODO: Implement shutdown hook
+            }
+        });
     }
 
     private void collect() {
