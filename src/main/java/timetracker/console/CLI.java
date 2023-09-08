d30c9a3afcbaa567725bf888f170c0463b888a88
Marlon Rosenberg <sceleton.smasher@gmail.com>
2023-09-08 15:31:23 +0200

    fix: naming issues

--- src/main/java/timetracker/console/CLI.java
+++ src/main/java/timetracker/console/CLI.java
@@ -89,7 +89,7 @@ public class CLI {
         dataReader.readAllTasks();
         dataReader.readAllTags();
         dataReader.readAllTimeIntervals();
-        dataReader.readMaxIDs();
+        dataReader.readInMaxIDsGlobal();
     }
 
     /**
