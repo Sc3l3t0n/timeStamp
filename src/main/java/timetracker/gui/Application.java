cc333aa65a2228c70e1367281c16fbf8b0c53556
Marlon Rosenberg <sceleton.smasher@gmail.com>
2023-09-09 20:09:40 +0200

    feat: reimplement shutdown hook

--- src/main/java/timetracker/gui/Application.java
+++ src/main/java/timetracker/gui/Application.java
@@ -1,12 +1,23 @@
 package timetracker.gui;
 
 import timetracker.API.DataReader;
+import timetracker.data.GlobalVariables;
 
 public class Application{
 
     public Application() {
         collect();
         MainForm mainForm = new MainForm();
+
+        // Shutdown hook if the application is closed while a task is running
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            GlobalVariables.ID_TO_TASK_MAP.forEach((id, task) -> {
+                if (task.isRunning()){
+                    System.out.println("Shutting down task " + id);
+                    task.stop();
+                }
+            });
+        }));
     }
 
     private void collect() {
